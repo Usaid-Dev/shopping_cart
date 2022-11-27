7a60bf3c619921fb9bed74a7196a52b98f71828b
Usaid Asif <usaidasif12@gmail.com>
2022-11-27 12:38:07 +0500

    Added cart-model

---
+++ lib/cart_model.dart
@@ -0,0 +1,44 @@
+class Cart {
+  late final int? id;
+  final String? productId;
+  final String? productName;
+  final int? initialPrice;
+  final int? productPrice;
+  final int? quantity;
+  final String? unitTag;
+  final String? image;
+
+  Cart(
+    this.id,
+    this.productId,
+    this.productName,
+    this.initialPrice,
+    this.productPrice,
+    this.quantity,
+    this.unitTag,
+    this.image,
+  );
+
+  Cart.fromMap(Map<dynamic, dynamic> res)
+      : id = res['id'],
+        productId = res['productId'],
+        productName = res['productName'],
+        initialPrice = res['initialPrice'],
+        productPrice = res['productPrice'],
+        quantity = res['quantity'],
+        unitTag = res['unitTag'],
+        image = res['image'];
+
+  Map<String, Object?> toMap() {
+    return {
+      'id': id,
+      'productId': productId,
+      'productName': productName,
+      'initialPrice': initialPrice,
+      'productPrice': productPrice,
+      'quantity': quantity,
+      'unitTag': unitTag,
+      'image': image
+    };
+  }
+}
